ddff877a708a1b5dcb7967c75e58a36db4a7f0ab
uigormarshall <uigor.silva@acad.ifma.edu.br>
2024-02-16 22:47:43 -0300

    start: Run Forrest, Run!

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+
+
+<template>
+  <div>
+    <a href="https:/dichavandobit.com" target="_blank">
+      <img src="./assets/logo.gif" class="logo" alt="Dichavando bit logo" />
+    </a>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  will-change: filter;
+  transition: filter 1500ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 0.5em #708028);
+}
+
+</style>
