202c7a6c104f46467949b47e3cacc13282f0cf68
uigormarshall <uigor.silva@acad.ifma.edu.br>
2024-02-23 20:24:54 -0300

    feat: adiciona componente CardPost

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,60 @@
 
 
 <template>
-  <div>
-    <a href="https:/dichavandobit.com" target="_blank">
-      <img src="./assets/logo.gif" class="logo" alt="Dichavando bit logo" />
-    </a>
+  <navbar></navbar>
+  <div class="container">
+    <div class="row col-md-9 mx-auto">
+      <div class="col">
+        <figure class="figure">
+          <img src="./assets/logo.gif" class="figure-img img-fluid rounded" alt="...">
+        </figure>
+        <card-post
+         title="Desafio 1: Mundo tá muito paradis?"
+         subtitle="Mussum Ipsum, cacilds vidis litro abertis. Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis! Nulla id gravida magna, ut semper sapien. Quem num gosta di mé, boa gentis num é. Casamentiss faiz malandris se pirulitá. "
+         published="21/99/9019"
+         ></card-post>
+         <card-post
+         title="Desafio 2: Casamentiss faiz malandris se pirulitá"
+         subtitle="Vide electram sadipscing et per. Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. Si num tem leite então bota uma pinga aí cumpadi!"
+         published="21/99/9019"
+         ></card-post>
+         <card-post
+         title="Desafio 3: Praesent malesuada urna nisi"
+         subtitle="Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Ô gente finis, pode baixar uma ampolis que hoje é sexta-feris!"
+         published="21/99/9019"
+         ></card-post>
+         <card-post
+         title="Desafio 4: Matis, aguis e fermentis"
+         subtitle="Copo furadis é disculpa de bebadis, arcu quam euismod magna. Nulla id gravida magna, ut semper sapien. Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis! Bota 1 metro de cachacis aí pra viagem!"
+         published="21/99/9019"
+         ></card-post>
+      </div>
+    </div>
+  </div>
+  <CardPost>
+    
+  </CardPost>
+  <div class="text-center footer"> 
+    © 2024 Copyright: <a href="https://dichavandobit.com"> Dichavandobit.com</a>
   </div>
 </template>
-
+<script>
+import CardPostVue from './components/CardPost.vue';
+import NavBar from './components/NavBar.vue';
+export default {
+  components: {
+    'card-post': CardPostVue,
+    'navbar': NavBar
+  }
+}
+</script>
 <style scoped>
 .logo {
   will-change: filter;
   transition: filter 1500ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 0.5em #708028);
 }
-
 </style>
---
+++ src/components/CardPost.vue
@@ -0,0 +1,44 @@
+<script setup>
+const props = defineProps(['title', 'subtitle', 'published'])
+
+</script>
+<template>
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">  {{ title }} </h5>
+                <p class="card-text">
+                    {{ subtitle }}
+                </p>
+                <h6>Publicado: {{ published }}</h6>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.card{
+  border: 2px solid #888831; 
+  border-style: groove ;
+  column-rule-style: inset;
+  background-color: #405010;
+  color: #d0d058;
+  margin-bottom: 10px;
+  height: auto;
+}
+.card-text{
+  color:  #d0d058;
+  font-size: 1rem;
+}
+.card-title{
+    border-bottom: 2px solid #888831; /* Espessura e cor da linha */
+    display: inline-block; /* Para que a linha não ocupe toda a largura */
+    margin-bottom: 10px; /* Espaçamento opcional abaixo do h6 */
+}
+.card:hover {
+      transform: scale(1.02); /* Aumenta o tamanho do card ao passar o mouse sobre ele */
+      transition: transform 0.4s ease; /* Adiciona uma transição suave */
+      background-color: #455711 ; 
+      border: 2px solid #d0d058; 
+}
+</style>
